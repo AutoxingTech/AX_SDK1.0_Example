1e0184e2d40044a0e7b9e330a9f69c594a9b1acd
chenlh <chenlh@mapbar.com>
2022-09-07 10:31:39 +0800

    fix: 去掉默认的configs信息

--- static/js/configs.js
+++ static/js/configs.js
@@ -1,5 +1,5 @@
 export const Configs = {
-  appId: 'axecb592405b534370',
-  appSecret: '2c7f69e36f734d859fc29a241b7dc6c0',
-  robotId: '71822011000140e'
+  appId: '',
+  appSecret: '',
+  robotId: ''
 }
