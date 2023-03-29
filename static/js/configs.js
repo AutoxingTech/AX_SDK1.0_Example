54d50da86c1fda7c5036d7bc4f110f757ee6fc54
wangmo <791993401@qq.com>
2023-03-29 14:27:38 +0800

    fix: 更新sdk版本

--- static/js/configs.js
+++ static/js/configs.js
@@ -3,5 +3,4 @@ export const Configs = {
   appSecret: '',
   robotId: '',
   fontUrl: './static/font/{fontstack}/{range}.pbf'
-  // fontUrl: 'http://localhost:8080/static/font/{fontstack}/{range}.pbf'
 }
