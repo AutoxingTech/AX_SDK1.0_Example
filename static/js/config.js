db81b17b7d11a92a9f2f095620a85310f073d21d
zhaogl1227 <zhaogl@autoxing.com>
2024-10-10 18:46:54 +0800

    fix: 欧洲版

--- static/js/config.js
+++ static/js/config.js
@@ -5,6 +5,11 @@ var Configs = {
   robotId: '',
   fontUrl: './static/font/{fontstack}/{range}.pbf',
   mode: 1, // 0: 国内，1海外, -1 测试
-  globalServicePath: 'https://devsdfsfdapi.autoxing.com/',
-  globalWsPath: 'wss://retail.autoxing.com/robot-control-dev/mapping'
+  globalServicePath: 'https://apieu.autoxing.com/',
+  globalWsPath: 'wss://serviceeu-dev.autoxing.com/'
+  // globalServicePath: 'https://devsdfsfdapi.autoxing.com/',
+  // globalWsPath: 'wss://retail.autoxing.com/robot-control-dev/mapping'
+  // mode: 1, // 0: 国内，1海外, -1 测试
+  // globalServicePath: 'https://devsdfsfdapi.autoxing.com/',
+  // globalWsPath: 'wss://retail.autoxing.com/robot-control-dev/mapping'
 }
